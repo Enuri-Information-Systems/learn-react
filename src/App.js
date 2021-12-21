8d903a59fa41bee23e83198311834d4affa3fa48
Pathum Kalhan <pathumsimpson@gmail.com>
2021-12-21 18:58:55 +0530

    plant

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const names = [
+    { text: "One", value: "One-1" },
+    {text:"Two-2",value:"Two-2"},
+    {text:"Three",value:"Three-3"},
+    {text:"Four",value:"Four-4"},
+    {text:"Five",value:"Five-5"},
+  ];
+  const [value, setValue] = useState();
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Value {value}
+      <select value={value} onChange={handleChange}>
+        <option>Please select a size</option>
+        {names.map((item) => (
+          <option key={item.value} value={item.value}>
+            {item.text}
+          </option>
+        ))}
+      </select>
+      {value && (
+        <img src={`/hedgerow-${value.split("-")[1]}.webp`} />
+
+      )}
     </div>
   );
 }
