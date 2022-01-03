4b21a73793da4e1759cb8774f8f54aec7370d101
Pathum Kalhan <pathumsimpson@gmail.com>
2022-01-03 09:00:35 +0530

    length convert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import convert from "convert-units";
 
 function App() {
+  const [initialValues, setInitialValues] = useState({
+    area: {
+      value: "100",
+      type: "cm",
+    },
+    length: {
+      value: "",
+      type: "",
+    },
+  });
+
+  const converter = (e, metric) => {
+    const widthType = e.target.value;
+    console.log("from",initialValues[metric])// 100, cm
+    console.log("to",widthType)// m
+
+    const r = convert(Number(initialValues[metric].value))
+      .from(initialValues[metric].type)
+      .to(widthType);
+      console.log("R",r)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select
+        onChange={(e) => {
+          e.persist();
+          converter(e, "area");
+          let newValues = {...initialValues};
+          newValues.area.type = e.target.value;
+          setInitialValues(newValues)
+        }}
+      >
+        <option value={'km'} key={'km'}>km</option>
+        <option value={'m'} key={'m'}>m</option>
+        <option value={'cm'} key={'cm'}>cm</option>
+      </select>
+      {JSON.stringify(initialValues)}
     </div>
   );
 }
