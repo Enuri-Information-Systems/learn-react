49c148a7b62ef6df71be7c8d4271d4f320a18d7b
Pathum Kalhan <pathumsimpson@gmail.com>
2022-02-08 14:51:38 +0530

    childeren branch

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import { useEffect, useState } from "react";
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>PDF test app</h1>
-      {/* <button>open the pdf</button> */}
-      <div>
-        <a href="https://chat-app-pathum.web.app/pdf/test.pdf" target="_blank" rel="noopener noreferrer">
-          chat app
-        </a>
+      <div className="main">
+        <div>
+          {/* <input type="Text" /> */}
+        </div>
+        <h1>Börsvärde</h1>
+        <p>
+        Omsättning
+        </p>
+        <p>Börsvärde</p>
       </div>
-      <div>
-        <a href="/pdf/test.pdf" target="_blank" rel="noopener noreferrer">
-          sample link
-        </a>
-      </div>
-      <div>
-        <a
-          href="http://www.africau.edu/images/default/sample.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hardcoded pdf
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hardcoded two
-        </a>
-      </div>
-     
-      
     </div>
   );
 }
