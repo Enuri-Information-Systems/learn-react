ab91e76ba2eb39435faebc2122224b195b2bf423
Pathum Kalhan <pathumsimpson@gmail.com>
2022-02-08 11:34:24 +0530

    parent and child

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>PDF test app</h1>
+      {/* <button>open the pdf</button> */}
+      <div>
+        <a href="https://chat-app-pathum.web.app/pdf/test.pdf" target="_blank" rel="noopener noreferrer">
+          chat app
+        </a>
+      </div>
+      <div>
+        <a href="/pdf/test.pdf" target="_blank" rel="noopener noreferrer">
+          sample link
+        </a>
+      </div>
+      <div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="http://www.africau.edu/images/default/sample.pdf"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hardcoded pdf
         </a>
-      </header>
+      </div>
+      <div>
+        <a
+          href="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hardcoded two
+        </a>
+      </div>
+     
+      
     </div>
   );
 }
---
+++ src/components/Parent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Parent({children}) {
+    console.log("children",children.length);
+  return <div>
+
+  </div>;
+}
