8bc3e149038a1bd30b45bc0a3524b7ec155c6f19
Pathum Kalhan <pathumsimpson@gmail.com>
2022-03-27 21:26:16 +0530

    formik clear errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Formik, Form, Field } from "formik";
+import React, { useState, useEffect, useRef } from "react";
+import * as Yup from "yup";
+import { Fragment } from "react/cjs/react.production.min";
 
 function App() {
+  const [initialValues, setInitialValues] = useState({
+    country: "Sri Lanka",
+  });
+  const validationSchema = Yup.object().shape({
+    country: Yup.string().required("Country is required"),
+  });
+
+  const [checked, setChecked] = useState();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Formik
+          initialValues={initialValues}
+          onSubmit={() => {}}
+          validationSchema={validationSchema}
+          enableReinitialize
         >
-          Learn React
-        </a>
+          {({
+            isValid,
+            dirty,
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            setErrors,
+          }) => {
+            return (
+              <Form>
+                <div>
+                    {checked?"checkbox":"non checked"}
+                  <Field type="text" name="country" />
+                </div>
+                <div>
+                  {checked ? (
+                    <Fragment>
+                      None of above{" "}
+                      <input
+                        type="checkbox"
+                        checked
+                        onChange={() => {
+                          setChecked(!checked);
+                          setErrors({});
+                        }}
+                      />
+                    </Fragment>
+                  ) : (
+                    <Fragment>
+                      None of above{" "}
+                      <input
+                        type="checkbox"
+                        
+                        onChange={() => {
+                          setChecked(!checked);
+                          setErrors({});
+                        }}
+                      />
+                    </Fragment>
+                  )}
+                </div>
+
+                <p>Errors : {JSON.stringify(errors)}</p>
+              </Form>
+            );
+          }}
+        </Formik>
       </header>
     </div>
   );
