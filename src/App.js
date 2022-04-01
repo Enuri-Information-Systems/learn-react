6c702a4932ea2921b7bf398c80f904e22b7ba824
Pathum Kalhan <pathumsimpson@gmail.com>
2022-04-01 11:04:05 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react';
+// import { Document, Page } from 'react-pdf';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import samplePDF from './test.pdf';
+
+// https://gist.github.com/wojtekmaj/f265f55e89ccb4ce70fbd2f8c7b1d95d
+function highlightPattern(text, pattern) {
+  const splitText = text.split(pattern);
+
+  if (splitText.length <= 1) {
+    return text;
+  }
+
+  const matches = text.match(pattern);
+
+  return splitText.reduce((arr, element, index) => (matches[index] ? [
+    ...arr,
+    element,
+    <mark key={index}>
+      {matches[index]}
+    </mark>,
+  ] : [...arr, element]), []);
 }
 
-export default App;
+export default function Test() {
+  const [searchText, setSearchText] = useState('');
+
+  const textRenderer = useMemo((textItem) => {
+    return highlightPattern(textItem.str, searchText);
+  }, [searchText]);
+
+  function onChange(event) {
+    setSearchText(event.target.value);
+  }
+
+  return (
+    <>
+      <Document
+        file={samplePDF}
+        // onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page
+          pageNumber={1}
+          // customTextRenderer={textRenderer}
+        />
+      </Document>
+      <div>
+        <label htmlFor="search">Search:</label>
+        <input type="search" id="search" value={searchText} onChange={onChange} />
+      </div>
+    </>
+  );
+}
