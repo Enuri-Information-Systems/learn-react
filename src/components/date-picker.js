8dcf19000bde3339f01396112140bfccd1dde3c5
pathum kalhan <pathumsimpson@gmail.com>
2022-05-14 09:39:52 +0530

    date picker

---
+++ src/components/date-picker.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+export default function DatePickerComponent() {
+    const [value, setValue] = React.useState<Date | null>(new Date());
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+        
+        <DatePicker
+          disableFuture
+          label="Responsive"
+          openTo="year"
+          views={['year', 'month', 'day']}
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+
+    </LocalizationProvider>
+  )
+}
